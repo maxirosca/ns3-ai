f667774b0f03f4efcd9cc8a438888a2f44e99001
maximilianrosca <maximilian.rosca@rwth-aachen.de>
2025-11-10 15:12:49 +0100

    Modify: Updated version of the transformer model

--- examples/nr-transformer/nr_transformer_model.py
+++ examples/nr-transformer/nr_transformer_model.py
@@ -6,16 +6,16 @@ import numpy as np
 
 
 class NrTransformerModel(nn.Module):
-    def __init__(self, d_model=16, nhead=2, num_layers=1, num_ues=4):
+    def __init__(self, d_model=64, nhead=4, num_layers=2, num_ues=4):
         super().__init__()
         self.num_ues = num_ues
-        self.embedding = nn.Linear(8, d_model)
+        self.embedding = nn.Linear(7, d_model)
         encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, batch_first=True)
         self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
         self.forward_layer = nn.Sequential(
-            nn.Linear(5 * d_model, 64),
+            nn.Linear(5 * d_model, 512),
             nn.ReLU(),
-            nn.Linear(64, 12 * num_ues)
+            nn.Linear(512, 12 * num_ues)
         )
 
     def forward(self, x):
@@ -36,9 +36,14 @@ class NrDataset(Dataset):
 
         slot_cols = ['simulation', 'frame', 'subframe', 'slot']
         features_cols = ["rnti", "resource_type", "priority", "delay_budget",
-             "hol_delay", "available_symbols", "queue_size", "mcs"]
+             "available_symbols", "queue_size", "mcs"]
         out_sym_cols = [f"sym{i}" for i in range(1, 13)]
 
+        flow_counts = self.input_data.groupby(slot_cols).size() # Drop the ~200 inputs with 6 features instead of 5
+        valid_slots = flow_counts[flow_counts == 5].index # Drop the ~200 inputs with 6 features instead of 5
+        self.input_data = self.input_data[self.input_data.set_index(slot_cols).index.isin(valid_slots)] # Drop the ~200 inputs with 6 features instead of 5
+        self.output_data = self.output_data[self.output_data.set_index(slot_cols).index.isin(valid_slots)] # Drop the ~200 inputs with 6 features instead of 5
+        self.output_data = self.output_data.drop_duplicates(subset=slot_cols, keep='first') # Drop the ~2000 duplicates from simulation 121
         self.output_indexed = self.output_data.set_index(slot_cols, drop=False)
         
         self.raw_features = []
@@ -49,11 +54,20 @@ class NrDataset(Dataset):
             self.raw_features.append(features)  
 
             label_row = self.output_indexed.loc[key]
-            labels = label_row[out_sym_cols].astype('float32').values
+            labels = label_row[out_sym_cols].astype('float32').values.flatten()
             self.raw_labels.append(labels)
- 
+
+        for i, f in enumerate(self.raw_features):
+            if f.shape != (5, 7):
+                print(f"Inconsistent feature shape at index {i}: {f.shape}")
+
+        for i, l in enumerate(self.raw_labels):
+            if l.shape != (12,):
+                print(f"Inconsistent label shape at index {i}: {l.shape}")
+        print("Features:", len(self.raw_features), "Labels:", len(self.raw_labels))
+
         # Convert to tensors
-        self.features = torch.tensor(np.stack(self.raw_features), dtype=torch.float32) # torch.size([5, 8])
+        self.features = torch.tensor(np.stack(self.raw_features), dtype=torch.float32) # torch.size([5, 7])
         self.labels = torch.tensor(np.stack(self.raw_labels), dtype=torch.long)  # torch.size([12])
 
     def __len__(self):
@@ -61,3 +75,8 @@ class NrDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.features[idx], self.labels[idx]
+    
+# pytorch_total_params = sum(p.numel() for p in NrTransformerModel().parameters())
+# pytorch_total_trainable_params = sum(p.numel() for p in NrTransformerModel().parameters() if p.requires_grad)
+# print(f"Total parameters: {pytorch_total_params}")
+# print(f"Total trainable parameters: {pytorch_total_trainable_params}")
