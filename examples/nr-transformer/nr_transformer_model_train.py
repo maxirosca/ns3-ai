27bfa58c9b1c5a66c906520b83f97e0ece91e4af
maximilianrosca <maximilian.rosca@rwth-aachen.de>
2026-01-05 22:07:34 +0200

    Add padding mask to train, validation and testing functions

--- examples/nr-transformer/nr_transformer_model_train.py
+++ examples/nr-transformer/nr_transformer_model_train.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from itertools import product
 import os
 import pandas as pd
+import numpy as np
 
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
@@ -46,28 +47,65 @@ try:
 except FileExistsError:
     pass  # File already exists
 
-csv_input_file = '~/maxi_model_training/training_dataset4/inputs_DlTransmission_zscore4.csv'
+# log_file_inference = 'inference_log_file.csv'
+# with open(log_file_inference, "w", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow([
+#         "inference_id",
+#         *[f"y_true_{i}" for i in range(12)],
+#         *[f"y_pred_{i}" for i in range(12)]
+#     ])
+
+csv_input_file = '~/Masterarbeit/training_dataset4/inputs_DlTransmission_zscore4.csv'
 csv_output_file = '~/maxi_model_training/training_dataset4/outputs_DlTransmission_no_duplicates4.csv'
 dataset = NrDataset(csv_input_file, csv_output_file)
 
-# hyperparameters_grid = {
-#     "lr": [5e-4, 3e-4, 1e-4, 5e-5],
-#     "d_model": [64, 128, 256, 512],
-#     "nhead": [2, 4, 8],
-#     "num_layers": [2, 3, 4, 5, 6],
-#     "batch_size": [16, 32, 64]
-# }
+# --------------------------------------------------------------------------------->
+# ---------------------------------- Split dataset in 3 sets ---------------------->
+# --------------------------------------------------------------------------------->
+N = len(dataset)
+indices_split = np.arange(N)
 
-# def generate_combinations(grid):
-#     keys = list(grid.keys())
-#     values = list(grid.values())
-#     for combination in product(*values):
-#         yield dict(zip(keys, combination))
+# Shuffle once
+rng = np.random.default_rng(seed=42)
+rng.shuffle(indices_split)
+
+# Define sizes
+size_10 = int(0.1 * N)
+size_33 = int(0.33 * N)
+size_66 = int(0.66 * N)
+
+# Nested subsets
+idx_10 = indices_split[:size_10]
+idx_33 = indices_split[:size_33]
+idx_66 = indices_split[:size_66]
+
+# Create the actual datasets
+dataset_10 = Subset(dataset, idx_10)
+dataset_33 = Subset(dataset, idx_33)
+dataset_66 = Subset(dataset, idx_66)
+# --------------------------------------------------------------------------------->
+# ---------------------------------- End split dataset in 3 sets ------------------>
+# --------------------------------------------------------------------------------->
+
+hyperparameters_grid = {
+    "lr": [5e-4, 3e-4, 1e-4, 5e-5],
+    "d_model": [64, 128, 256, 512],
+    "nhead": [2, 4, 8],
+    "num_layers": [2, 3, 4, 5, 6],
+    "batch_size": [16, 32, 64]
+}
+
+def generate_combinations(grid):
+    keys = list(grid.keys())
+    values = list(grid.values())
+    for combination in product(*values):
+        yield dict(zip(keys, combination))
 
 num_samples = len(dataset)
 indices = list(range(num_samples))
-train_idx, temp_idx = train_test_split(indices, test_size=0.3, random_state=42, shuffle=True)
-val_idx, test_idx = train_test_split(temp_idx, test_size=0.3, random_state=42, shuffle=True)
+train_idx, temp_idx = train_test_split(indices, test_size=0.4, random_state=42, shuffle=True)
+val_idx, test_idx = train_test_split(temp_idx, test_size=0.5, random_state=42, shuffle=True)
 
 train_dataset = Subset(dataset, train_idx)
 val_dataset = Subset(dataset, val_idx)
@@ -86,6 +124,10 @@ def run_training(hparams):
     # Initialize model, loss function, and optimizer
     # model_params = torch.load('/home/maximilianrosca/ns-3-dev/contrib/ai/examples/nr-transformer/nr_transformer_model.pth')
     model = NrTransformerModel(d_model=hparams["d_model"], nhead=hparams["nhead"], num_layers=hparams["num_layers"]).to(device)
+    pytorch_total_params = sum(p.numel() for p in model.parameters())  
+    pytorch_total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Total parameters: {pytorch_total_params}")
+    print(f"Total trainable parameters: {pytorch_total_trainable_params}")
     # model.load_state_dict(model_params['model_state_dict'])
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=hparams["lr"])
@@ -94,36 +136,42 @@ def run_training(hparams):
     # Training loop
     best_val_loss = float('inf')
     # best_val_loss = model_params['val_loss']
-    # patience, patience_counter = 5, 0
+    patience, patience_counter = 5, 0
     num_epochs = 5
-    # model_name = (
-    #     f"nr_transformer_model_best_lr{hparams['lr']}_dmodel{hparams['d_model']}"
-    #     f"_nhead{hparams['nhead']}_layers{hparams['num_layers']}"
-    #     f"_bs{hparams['batch_size']}.pth"
-    # )
     # num_epochs = model_params['epoch']
+    model_path = (
+         f"best-model-params/"
+         f"nr_transformer_model_best_lr{hparams['lr']}_dmodel{hparams['d_model']}"
+         f"_nhead{hparams['nhead']}_layers{hparams['num_layers']}"
+         f"_bs{hparams['batch_size']}.pth"
+    )
+    model_name = (
+         f"nr_transformer_model_best_lr{hparams['lr']}_dmodel{hparams['d_model']}"
+         f"_nhead{hparams['nhead']}_layers{hparams['num_layers']}"
+         f"_bs{hparams['batch_size']}.pth"
+    )
     for t in range(num_epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train_loss, train_acc = train(train_loader, model, loss_fn, optimizer)
         val_loss, val_acc = validation(val_loader, model, loss_fn)
         log_training_validation(t, train_loss, train_acc, val_loss, val_acc, hparams)
-        # if val_loss < best_val_loss:
-        #     best_val_loss = val_loss
-        #     patience_counter = 0
-        #     torch.save(model.state_dict(), model_name)
-        #     print("New best model saved!")
-        # else:
-        #     patience_counter += 1
-        #     if patience_counter >= patience:
-        #         print("Early stopping triggered.")
-        #         break
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            patience_counter = 0
+            torch.save(model.state_dict(), model_path)
+            print("New best model saved!")
+        else:
+            patience_counter += 1
+            if patience_counter >= patience:
+                print("Early stopping triggered.")
+                break
     
-    # # Model testing
-    # print("Testing the best model on the test set!")
-    # model.load_state_dict(torch.load("nr_transformer_model_best.pth"))
-    # test_loss, test_acc = test(test_loader, model, loss_fn, hparams)
-    # log_test(test_loss, test_acc)
-    # print("Testing completed!")
+    # Model testing
+    print("Testing the best model on the test set!")
+    model.load_state_dict(torch.load(model_path))
+    test_loss, test_acc = test(test_loader, model, loss_fn)
+    log_test(test_loss, test_acc, hparams)
+    print("Testing completed!")
 
 def train(train_loader, model, loss_fn, optimizer):
     total_loss = 0.0
@@ -132,7 +180,8 @@ def train(train_loader, model, loss_fn, optimizer):
     for X, y in train_loader:
         X, y = X.to(device), y.to(device)
         # Forward pass and compute loss
-        pred = model(X)
+        pad_mask = (X[..., 0] == 0)
+        pred = model(X, src_key_padding_mask=pad_mask)
         # print(f"Model output shape before permute: {pred.shape}")
         pred = pred.permute(0, 2, 1)
         # print(f"Model output shape after permute: {pred.shape}")
@@ -159,7 +208,8 @@ def validation(val_loader, model, loss_fn):
     with torch.no_grad():
         for X, y in val_loader:
             X, y = X.to(device), y.to(device)
-            pred = model(X)
+            pad_mask = (X[..., 0] == 0)
+            pred = model(X, src_key_padding_mask=pad_mask)
             pred = pred.permute(0, 2, 1)
             loss = loss_fn(pred, y)
             total_loss += loss.item()
@@ -175,17 +225,32 @@ def validation(val_loader, model, loss_fn):
 def test(test_loader, model, loss_fn):
     total_loss = 0.0
     total_accuracy = 0.0
+    #inference_id = 0
     model.eval()
     with torch.no_grad():
         for X, y in test_loader:
             X, y = X.to(device), y.to(device)
-            pred = model(X)
+            pad_mask = (X[..., 0] == 0)
+            pred = model(X, src_key_padding_mask=pad_mask)
             pred = pred.permute(0, 2, 1)
             loss = loss_fn(pred, y)
             total_loss += loss.item()
             predicted_classes = pred.argmax(dim=1)
             correct = (predicted_classes == y).float()
             total_accuracy += correct.mean().item()
+
+            # # Move to CPU once
+            # y_cpu = y.cpu().numpy()
+            # preds_cpu = predicted_classes.cpu().numpy()
+            # with open(log_file_inference, "a", newline="") as f:
+            #     writer = csv.writer(f)
+            #     for i in range(y_cpu.shape[0]):
+            #         writer.writerow([
+            #             inference_id,
+            #             *y_cpu[i].tolist(),
+            #             *preds_cpu[i].tolist()
+            #         ])
+            #         inference_id += 1
 
     avg_loss = total_loss / len(test_loader)
     avg_accuracy = total_accuracy / len(test_loader)
@@ -221,25 +286,33 @@ def log_test(test_loss, test_acc, hparams):
                round(test_acc*100, 2)]
         writer.writerow(row)
 
-# for hparams in generate_combinations(hyperparameters_grid):
-#     config_tuple = (
-#         hparams["lr"],
-#         hparams["d_model"],
-#         hparams["nhead"],
-#         hparams["num_layers"],
-#         hparams["batch_size"]
-#     )
-#     if config_tuple in trained_configs:
-#         print(f"Skipping already trained configuration: {hparams}")
-#         continue
-#     print(f"Running training with hyperparameters: {hparams}")
-#     run_training(hparams)
+for hparams in generate_combinations(hyperparameters_grid):
+    config_tuple = (
+        hparams["lr"],
+        hparams["d_model"],
+        hparams["nhead"],
+        hparams["num_layers"],
+        hparams["batch_size"]
+    )
+    if config_tuple in trained_configs:
+        print(f"Skipping already trained configuration: {hparams}")
+        continue
+    print(f"Running training with hyperparameters: {hparams}")
+    run_training(hparams)
 
-hparams = {
-    "lr": 0.0005,
-    "d_model": 64,
-    "nhead": 8,
-    "num_layers": 2,
-    "batch_size": 32
-}
-run_training(hparams)
+# hparams = {
+#     "lr": 0.0005,
+#     "d_model": 64,
+#     "nhead": 8,
+#     "num_layers": 2,
+#     "batch_size": 32
+# }
+# run_training(hparams)
+
+# # ----------------------------------------------------------
+# test_loader = DataLoader(test_dataset, batch_size= 32, shuffle=False)
+# model = NrTransformerModel(d_model=16, nhead=2, num_layers=1, num_ues=4).to(device)
+# model.load_state_dict(torch.load('nr_transformer_model_best_lr0.0001_dmodel16_nhead2_layers1_bs32.pth'))
+# loss_fn = nn.CrossEntropyLoss()
+# test_loss, test_acc = test(test_loader, model, loss_fn)
+# # ----------------------------------------------------------
